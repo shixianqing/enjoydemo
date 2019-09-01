163471f60e70f416fc0aae9a9969c63d8cd3134a
shixianqing <1434501995@qq.com>
2019-09-01 17:53:56 +0800

    提交

---
+++ spring-demo/src/main/java/com/example/lifecycle/bean/Train.java
@@ -0,0 +1,66 @@
+package com.example.lifecycle.bean;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+/**
+ * *                            _ooOoo_
+ * *                           o8888888o
+ * *                           88" . "88
+ * *                           (| -_- |)
+ * *                            O\ = /O
+ * *                        ____/`---'\____
+ * *                      .   ' \\| |// `.
+ * *                       / \\||| : |||// \
+ * *                    / _||||| -:- |||||- \
+ * *                       | | \\\ - /// | |
+ * *                     | \_| ''\---/'' | |
+ * *                      \ .-\__ `-` ___/-. /
+ * *                   ___`. .' /--.--\ `. . __
+ * *                ."" '< `.___\_<|>_/___.' >'"".
+ * *               | | : `- \`.;`\ _ /`;.`/ - ` : | |
+ * *                 \ \ `-. \_ __\ /__ _/ .-` / /
+ * *         ======`-.____`-.___\_____/___.-`____.-'======
+ * *                            `=---='
+ * *
+ * *         .............................................
+ * *                  佛祖保佑             永无BUG
+ * *          佛曰:
+ * *                  写字楼里写字间，写字间里程序员；
+ * *                  程序人员写程序，又拿程序换酒钱。
+ * *                  酒醒只在网上坐，酒醉还来网下眠；
+ * *                  酒醉酒醒日复日，网上网下年复年。
+ * *                  但愿老死电脑间，不愿鞠躬老板前；
+ * *                  奔驰宝马贵者趣，公交自行程序员。
+ * *                  别人笑我忒疯癫，我笑自己命太贱；
+ * *                  不见满街漂亮妹，哪个归得程序员？
+ *
+ * @Author:shixianqing
+ * @Date:2019/9/1 16:24
+ * @Description:
+ **/
+@Component
+public class Train implements InitializingBean, DisposableBean {
+
+    public Train(){
+        System.out.println("Train .......... construtor........");
+    }
+
+    /**
+     * 自定义初始化方法会在afterPropertiesSet方法执行后执行
+     */
+    public void init(){
+        System.out.println("Train............... init ...........");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("Train .......... destroy .............");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Train .......... afterPropertiesSet .............");
+    }
+}
---
+++ spring-demo/src/main/java/com/example/lifecycle/config/BeanLifeCycleConfig.java
@@ -0,0 +1,61 @@
+package com.example.lifecycle.config;
+
+import com.example.lifecycle.bean.BiKe;
+import com.example.lifecycle.bean.Train;
+import com.example.model.Person;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * *                            _ooOoo_
+ * *                           o8888888o
+ * *                           88" . "88
+ * *                           (| -_- |)
+ * *                            O\ = /O
+ * *                        ____/`---'\____
+ * *                      .   ' \\| |// `.
+ * *                       / \\||| : |||// \
+ * *                    / _||||| -:- |||||- \
+ * *                       | | \\\ - /// | |
+ * *                     | \_| ''\---/'' | |
+ * *                      \ .-\__ `-` ___/-. /
+ * *                   ___`. .' /--.--\ `. . __
+ * *                ."" '< `.___\_<|>_/___.' >'"".
+ * *               | | : `- \`.;`\ _ /`;.`/ - ` : | |
+ * *                 \ \ `-. \_ __\ /__ _/ .-` / /
+ * *         ======`-.____`-.___\_____/___.-`____.-'======
+ * *                            `=---='
+ * *
+ * *         .............................................
+ * *                  佛祖保佑             永无BUG
+ * *          佛曰:
+ * *                  写字楼里写字间，写字间里程序员；
+ * *                  程序人员写程序，又拿程序换酒钱。
+ * *                  酒醒只在网上坐，酒醉还来网下眠；
+ * *                  酒醉酒醒日复日，网上网下年复年。
+ * *                  但愿老死电脑间，不愿鞠躬老板前；
+ * *                  奔驰宝马贵者趣，公交自行程序员。
+ * *                  别人笑我忒疯癫，我笑自己命太贱；
+ * *                  不见满街漂亮妹，哪个归得程序员？
+ *
+ * @Author:shixianqing
+ * @Date:2019/9/1 13:32
+ * @Description:
+ * bean的生命周期：创建---->>>>初始化  --------->>>> 销毁过程
+ **/
+@Configuration
+@ComponentScan("com.example.lifecycle.bean")
+public class BeanLifeCycleConfig {
+
+
+    @Bean(initMethod = "init",destroyMethod = "destroy")
+    public BiKe biKe(){
+        return new BiKe();
+    }
+
+//    @Bean(initMethod = "init")
+    public Train train(){
+        return new Train();
+    }
+}
